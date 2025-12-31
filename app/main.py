9a4cd5ec860a9bb262a64765eb2fc154a91efe9b
ArtemZakhar <artemzakharchuk@gmail.com>
2025-12-31 09:42:22 +0200

    using zip

--- app/main.py
+++ app/main.py
@@ -10,11 +10,11 @@ class Person:
 def create_person_list(people: list) -> list:
     new_persons = [Person(person["name"], person["age"]) for person in people]
 
-    for index, new_person in enumerate(new_persons):
-        person_data = people[index]
-        if person_data.get("wife"):
-            new_person.wife = Person.people[person_data["wife"]]
-        if person_data.get("husband"):
-            new_person.husband = Person.people[person_data["husband"]]
+    for person in people:
+        person_data = Person.people[person["name"]]
+        if person.get("wife"):
+            person_data.wife = Person.people[person["wife"]]
+        if person.get("husband"):
+            person_data.husband = Person.people[person["husband"]]
 
     return new_persons
